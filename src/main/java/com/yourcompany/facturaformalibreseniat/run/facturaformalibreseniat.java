a1140271312e408945f0d78bca6f5b24548e2933
Jesus Aquino <jaqconsultor@gmail.com>
2023-11-23 22:51:20 -0400

    m

--- src/main/java/com/yourcompany/facturaformalibreseniat/run/facturaformalibreseniat.java
+++ src/main/java/com/yourcompany/facturaformalibreseniat/run/facturaformalibreseniat.java
@@ -11,7 +11,7 @@ import org.openxava.util.*;
 public class facturaformalibreseniat {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("facturaformalibreseniat-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		//DBServer.start("facturaformalibreseniat-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
 		AppServer.run("facturaformalibreseniat"); // Usa AppServer.run("") para funcionar en el contexto raíz
 	}
 
